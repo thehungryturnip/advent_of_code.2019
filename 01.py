2be55e5bc5deebc31a7feda73d9ffe68bfe0d30a
thehungryturnip <thehungryturnip@gmail.com>
2019-12-01 20:35:02 -0500

    Completed Day 01 Part B.

--- 01.py
+++ 01.py
@@ -8,11 +8,26 @@
 
 import sys
 
+def formula_a(weight):
+    return weight // 3 - 2
 
-fuel = 0
+def formula_b(weight):
+    total = 0
+    fuel = formula_a(weight)
+    while fuel > 0:
+        total += fuel
+        fuel = formula_a(fuel)
+    print(f'{weight}:{total}')
+    return total
+
+fuel_a = 0
+fuel_b = 0
 with open(sys.argv[1], 'r') as f:
     for l in f:
-        # print(int(l) // 3 - 2)
-        fuel += int(l) // 3 - 2
+        fuel_a += formula_a(int(l))
+        # print(fuel_a)
+        fuel_b += formula_b(int(l))
+        # print(fuel_b)
 
-print(f'[01a] Total fuel cost is {fuel}.')
+print(f'[01a] Total fuel cost is {fuel_a}.')
+print(f'[01b] Total fuel cost is {fuel_b}.')
